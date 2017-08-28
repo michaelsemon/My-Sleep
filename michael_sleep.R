bbe2a003d0c22ea65ec853ae9052f1d638ffb498
michaelsemon <michaelhsemon@gmail.com>
2017-08-27 19:54:23 -0700

    Update michael_sleep.R

--- michael_sleep.R
+++ michael_sleep.R
@@ -7,11 +7,9 @@ library(tidyr)
 library(lubridate)
 library(MASS)
 library(caTools)
-library(neuralnet)
-library(scales)
 
+## data clean / prep ##
 
-# data clean / prep
 sleep <- read.csv('sleepdata.csv',sep = ';')
 sleep[,3:5] <- sapply(sleep[,3:5],as.numeric)
 
@@ -19,11 +17,22 @@ sleep <- as.data.frame(sleep) %>% subset(sleep$Sleep.quality > 5)
 
 sleep$Start <- as.POSIXct(sleep$Start,format= '%Y-%m-%d %H:%M:%S') #2014-07-30 23:12:48
 sleep$End <- as.POSIXct(sleep$End,format= '%Y-%m-%d %H:%M:%S')
-sleep$Sleep.time <- (sleep$End - sleep$Start)/60 # csv error, could not get time.in.bed
+sleep$Sleep.time <- ((sleep$End - sleep$Start)/60) %>% as.numeric # csv error, could not get time.in.bed
 sleep$hour <- hour(sleep$Start) #create hour column
 
-# explore 
+## explore / visualizations ##
 
+sleep.numeric <- sapply(sleep,is.numeric) %>% as.matrix
+sleep.numeric <- sleep[,sleep.numeric]
+sleep.cor <- cor(sleep.numeric)
+#print(corrplot(sleep.cor,method='circle'))
 
-pl1 <- ggplot(sleep,aes(Start,Sleep.quality)) + geom_point(aes(color=Sleep.time)) + scale_color_gradient(low='blue',high='green')
+pl1 <- ggplot(sleep,aes(Start,Sleep.quality)) + geom_point(aes(color=Sleep.time)) 
+pl1 <- pl1 + stat_smooth() + theme_classic() + scale_color_gradient(low='blue',high='green')
+pl1 <- pl1 + theme(legend.title = "Hours in Bed")
 print(pl1)
+
+
+
+
+
